a218c04a4ac3fcc5b9c0cfe79c71c034969a8131
HashmatG <cxm3gt@virginia.edu>
2025-08-03 15:54:18 -0400

    Resume FeedBack Page

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,6 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
+    route('/resume/:id', 'routes/resume.tsx'),
 
 ] satisfies RouteConfig;
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -174,16 +174,10 @@ const Upload = () => {
 
 
         data.feedback = JSON.parse(feedbackText);
-
-
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
-
-
         setStatusText('Analysis complete, redirecting...');
-
-
         console.log(data);
-
+        navigate(`/resume/${uuid}`);
 
     }
 
